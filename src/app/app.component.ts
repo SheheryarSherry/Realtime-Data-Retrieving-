6369bbe7760e49a2ef11d4a713bef2b5954a68d2
Sheheryar Khan <Sheheryar Khan>
2017-01-02 02:01:47 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  items:FirebaseListObservable<any[]>;
+  constructor(af:AngularFire){
+this.items=af.database.list('/items');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {AngularFireModule} from 'angularfire2';
 import { AppComponent } from './app.component';
-
+const firebaseConfig = {
+ apiKey: "AIzaSyAxik2SYOYkTyq5dOqK0x2Aq9J9tPUReic",
+    authDomain: "list-fetch.firebaseapp.com",
+    databaseURL: "https://list-fetch.firebaseio.com",
+    storageBucket: "list-fetch.appspot.com",
+    messagingSenderId: "646108505042"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
